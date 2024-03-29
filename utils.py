cc86327a1cf30390a11feac1d7727b44250b9daa
qhy040404 <qhy040404@163.com>
2024-03-29 18:20:01 +0800

    Revert "Revert "Use bot's PAT to create release and merge docs""
    This reverts commit e7cae317156827a50fe4267f060b483002296d80.

--- utils.py
+++ utils.py
@@ -16,25 +16,3 @@ def download_stream_file(url, file_name, headers=None):
         with open(f"./cache/{file_name}", "wb") as f:
             for chunk in r.iter_bytes():
                 f.write(chunk)
-
-
-def send_zulip_message(message: str):
-    zulip_url = "https://hutao.zulipchat.com/api/v1/messages"
-    BOT_EMAIL_ADDRESS = os.getenv("ZULIP_EMAIL")
-    BOT_API_KEY = os.getenv("ZULIP_API_KEY")
-    zulip_stream = os.getenv("ZULIP_STREAM")
-    zulip_topic = os.getenv("ZULIP_TOPIC")
-
-    data = {
-        "type": "stream",
-        "to": zulip_stream,
-        "topic": zulip_topic,
-        "content": message
-    }
-
-    client = httpx.Client(auth=(BOT_EMAIL_ADDRESS, BOT_API_KEY))
-
-    response = client.post(zulip_url, data=data)
-    print(response.status_code)
-    print(response.text)
-    return True
