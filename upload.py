5840db911ef8c8453d8a91c3fb67dd5e7758c8a8
qhy040404 <qhy040404@163.com>
2025-03-30 14:37:14 +0800

    upload before release

---
+++ upload.py
@@ -0,0 +1,30 @@
+import os
+import sys
+
+import boto3
+from botocore.config import Config
+
+filename = sys.argv[1]
+
+config = Config(signature_version='s3v4')
+s3_client = boto3.client(
+    's3',
+    aws_access_key_id=os.getenv("S3_ACCESS_KEY"),
+    aws_secret_access_key=os.getenv("S3_SECRET_KEY"),
+    endpoint_url=os.getenv("S3_ENDPOINT"),
+    config=config
+)
+bucket_name = "hutao-distribute"
+print("Uploading to hutao-dist R2 bucket...")
+s3_client.upload_file(filename, bucket_name, filename)
+
+minio_s3_client = boto3.client(
+    's3',
+    aws_access_key_id=os.getenv("MINIO_ACCESS_KEY"),
+    aws_secret_access_key=os.getenv("MINIO_SECRET_KEY"),
+    endpoint_url=os.getenv("MINIO_ENDPOINT"),
+    config=config
+)
+minio_bucket_name = "hutao"
+print("Uploading to hutao-dist MinIO bucket...")
+minio_s3_client.upload_file(filename, minio_bucket_name, filename)
